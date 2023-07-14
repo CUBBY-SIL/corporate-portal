6841bf51e58eeaee73e28b17a1f25afca2708f2b
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-15 06:44:43 +0700

    Добавил таблицу "Реестр"

--- sections/section1.php
+++ sections/section1.php
@@ -14,5 +14,32 @@
             
         </div>
 
+        <div class="table_box">
+                <table style="border: 1px solid #eee;table-layout: fixed;width: 100%;margin-bottom: 20px;" id="tbl_rer" name="tbl_rer">
+                    <thead>
+                        <tr>
+                            <th>ID_Записи</th>
+                            <th>Номер договора</th>
+                            <th>Дата</th>
+                            <th style="width:300px;">Предмет</th>
+                            <th>Контрагент</th>
+                            <th>ИНН</th>
+                            <th>Цена договора согласована</th>
+                            <th>Цена договора заключенный</th>
+                            <th>КВР</th>
+                            <th>КОСГУ</th>
+                            <th>Источник финасирования</th>
+                            <th>Филиал</th>
+                            <th>Письмо согласования</th>
+                            <th>Дата письма согласования</th>
+                            <th>ПФХД</th>   
+                            <th>Оплачено</th>   
+                            <th>Остаток</th>   
+                            <th>Примечание</th>   
+                        </tr>
+                    </thead>
+                </table>
+        </div><!-- END DIV TABLE BOX -->
+
     </body> 
 </html>
