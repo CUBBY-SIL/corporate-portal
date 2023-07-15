67fe8de0920db1db88e557238aca72d0a7af2ccb
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-15 07:17:08 +0700

    Добавил таблицу "Чеки"

--- sections/section4.php
+++ sections/section4.php
@@ -13,5 +13,64 @@
             
         </div>
 
+        <div class="table_box">
+
+            <table style="border: 1px solid #eee;table-layout: fixed;width: 100%;margin-bottom: 20px;" id="tbl_rcps" name="tbl_rcps">
+                <thead>            
+                    <tr>      
+                        <th>ID_Записи</th> 
+                        <th>КВР</th>      
+                        <th>КОСГУ</th>          
+                        <th>Источник финансирования</th>        
+                        <th>ПФХД</th>      
+                        <th>Филиал</th>        
+                        <th>Сумма, руб</th> 
+                    </tr>
+                </thead>
+            </table>
+                    
+            <script>
+                $(document).ready(function() {
+                    $.ajax({
+                        url: './php/fetch_data4.php',
+                        method: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            var newLine = $('#tbl_rcps');
+                            
+                            newLine.append('<tbody>');
+                            
+                            var field = data;
+                            var trHTML = '';
+
+                            $.each(data, function(index, value) {
+                                trHTML += '<tr><td>' + 
+                                    value.id + '</td><td>' + 
+                                    value.kvr + '</td><td>' + 
+                                    value.kosgu + '</td><td>' + 
+                                    value.financ + '</td><td>' + 
+                                    value.pvhd + '</td><td>' + 
+                                    value.filial + '</td><td>' + 
+                                    value.sum + '</td></tr>';
+                            });
+                            newLine.append(trHTML);
+                            newLine.append('</tbody>');
+
+                            $("#tbl_rcps").trigger('updateCache');
+                            $("#tbl_rcps").tablesorter();
+                        },
+                        error: function(xhr, status, error) {
+                        console.log(xhr.responseText);
+                        }
+                    }); 
+                });
+
+                $(document).ready(function() 
+                    { 
+                            $("#tbl_rcps").tablesorter(); 
+                    } 
+                );
+            </script>
+        </div><!-- END DIV TABLE BOX -->
     </body>
 </html>
