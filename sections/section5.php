2dc714cc299afe8d10c15a0c42b5718e0b679948
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-15 07:24:45 +0700

    Добавил таблицу "Эл. приемная"

--- sections/section5.php
+++ sections/section5.php
@@ -13,5 +13,62 @@
             
         </div>
 
+        <div class="table_box" style="overflow-x: hidden;">
+
+            <table style="border: 1px solid #eee;table-layout: fixed;width: 100%;margin-bottom: 20px;" id="tbl_elfbu" name="tbl_elfbu">
+                <thead>
+                    <tr>      
+                        <th>ID_Записи</th>
+                        <th>Дата</th>     
+                        <th>№ контракта</th>         
+                        <th style="width:400px;">Наименование</th>       
+                        <th>Срок</th>       
+                        <th>Факт</th>     
+                    </tr>
+                </thead>
+            </table>
+                
+            <script>
+                $(document).ready(function() {
+                    $.ajax({
+                        url: './php/fetch_data5.php',
+                        method: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            var newLine = $('#tbl_elfbu');
+                            
+                            newLine.append('<tbody>');
+                            
+                            var field = data;
+                            var trHTML = '';
+
+                            $.each(data, function(index, value) {
+                                trHTML += '<tr><td>' + 
+                                    value.id + '</td><td>' + 
+                                    value.date + '</td><td>' + 
+                                    value.name_contr + '</td><td>' + 
+                                    value.name + '</td><td>' + 
+                                    value.term + '</td><td>' + 
+                                    value.fact + '</td></tr>';
+                            });
+                            newLine.append(trHTML);
+                            newLine.append('</tbody>');
+
+                            $("#tbl_elfbu").trigger('updateCache');
+                            $("#tbl_elfbu").tablesorter();
+                        },
+                        error: function(xhr, status, error) {
+                        console.log(xhr.responseText);
+                        }
+                    }); 
+                });
+
+                $(document).ready(function() 
+                    { 
+                            $("#tbl_elfbu").tablesorter(); 
+                    } 
+                );
+            </script>
+        </div><!-- END DIV TABLE BOX -->
     </body>
 </html>
