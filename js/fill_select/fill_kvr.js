3550e35273d78044c7cf2e450a5effdc331e23bb
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-16 13:21:55 +0700

    AJAX запрос для заполнения списка КВР

---
+++ js/fill_select/fill_kvr.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $.ajax({
+        url: './php/KVR.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var dropdown = $('#reest-add-sel_kvr');
+            dropdown.empty();
+            
+            var fixOpt = '';
+            fixOpt = '<option disabled selected value="0">' + 'Выберите КВР' + '</option>' 
+            
+            dropdown.append(fixOpt);
+
+            $.each(data, function(index, value) {
+                dropdown.append($('<option></option>').attr('value', value.id).text(value.name));
+            });
+        },
+        error: function(xhr, status, error) {
+        console.log(xhr.responseText);
+        }
+    });
+});
