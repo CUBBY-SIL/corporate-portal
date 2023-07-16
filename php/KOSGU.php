9e80598cc28c0ead3549a7a241e58c484cae2200
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-16 12:37:51 +0700

    PHP код для извлечения строк из таблицы КОСГУ

---
+++ php/KOSGU.php
@@ -0,0 +1,31 @@
+<?php
+    $host='localhost';
+    $database='reestr';
+    $user='root';
+    $password='';
+
+    $conn = mysqli_connect($host, $user, $password, $database);
+
+    // Проверка соединения
+    if ($conn->connect_error) {
+        die("Ошибка подключения к базе данных: " . $conn->connect_error);
+    }
+
+    // Выполнение запроса для получения данных из таблицы
+    $query =  "SELECT `id`, `name` FROM KOSGU";
+    $result = mysqli_query($conn, $query);
+    // Формирование массива данных клиентов
+    $kosgu = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $kosgu[] = array(
+            'id' => $row["id"],
+            'name' => $row["name"]
+        );
+    }
+
+    // Закрытие соединения с базой данных
+    mysqli_close($conn);
+
+    // Вывод данных в формате JSON
+    echo json_encode($kosgu);
+?>
