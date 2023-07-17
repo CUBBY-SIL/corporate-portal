92a3e229f5db516355966d0dc9ff84c6de3c6f5f
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-17 16:58:48 +0700

    Добавил кнопки для вызова форм обратной связи

--- sections/section5.php
+++ sections/section5.php
@@ -8,9 +8,27 @@
         
         <script src="./js/jquery-3.6.1.js"></script>
         <script src="./js/jquery.tablesorter.js"></script>
+        <script src="./js/show_hide_form.js"></script>
         
         <div class="inner_content">
-            
+            <div id="form_box">
+                <div class="btn_box">
+                    <div id="btn_add">
+                        <button id="btn_ad" class="submit1" >Добавить запись</button>
+                    </div>
+
+                    <div id="btn_edit">
+                        <button id="btn_ed" class="submit1" >Редактировать запись</button>
+                    </div>
+
+                    <div id="btn_delete">
+                        <button id="btn_del" class="submit1" >Удалить запись</button>
+                    </div>
+                </div><br/><!-- END DIV BTN BOX -->
+
+            </div><!-- END DIV FORM BOX -->    
+
+            <h2>Электронная приемная ФБУ</h2>
         </div>
 
         <div class="table_box" style="overflow-x: hidden;">
