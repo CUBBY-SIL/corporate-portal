51685edaaaa3b32547127de5e8788484b6ed9f16
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-17 23:36:21 +0700

    Добавил формы обратной связи

--- sections/section9.php
+++ sections/section9.php
@@ -9,6 +9,7 @@
         <script src="./js/jquery-3.6.1.js"></script>
         <script src="./js/jquery.tablesorter.js"></script>
         <script src="./js/fill_table/fill_users.js"></script>
+        <script src="./js/show_hide_form.js"></script>
         
         <div class="inner_content">
             <div id="form_box">
@@ -26,6 +27,76 @@
                     </div>
                 </div><br/><!-- END DIV BTN BOX -->
 
+                <div id="form_add_edit" style="display:none;">
+
+                    <h2 id="h_add" style="display:none;">Добавить пользователя</h2>
+                    <h2 id="h_edit" style="display:none;">Редактировать данные</h2>
+
+                        <form name="form-add_user" id="form-add_user">
+
+                            <div id="edit_num_row" class="inner-block-add" style="margin-bottom:25px; display:none;">
+                                <label for="reestr-edit-num_row" class="label-style" style="text-align: left; margin-left:8px;">ID Записи:</label>
+                                <input class="input_block" type="text" name="reestr-edit-num_row" id="reestr-edit-num_row" placeholder="Введите номер" />
+                            </div>
+                            
+                            <div class="block-add" >
+                                <div class="inner-block-add">
+                                    <label for="user-add-login" class="label-style">Логин:</label>
+                                    <input class="input_block" type="text" name="user-add-login" id="user-add-login" placeholder="Введите логин" />
+                                </div>
+                                
+                                <div class="inner-block-add">
+                                    <label for="user-add-pass" class="label-style">Пароль:</label>
+                                    <input class="input_block" type="text"  name="user-add-pass" id="user-add-pass" placeholder="Введите пароль"/>
+                                </div>
+                                    
+                                <div class="inner-block-add">
+                                    <label for="user-add-name" class="label-style">Имя:</label>
+                                    <input class="input_block" type="text"  name="user-add-name" id="user-add-name" placeholder="Введите имя"/>
+                                </div>
+                                    
+                                <div class="inner-block-add">
+                                    <label for="user-add-mid_name" class="label-style">Отчество:</label>
+                                    <input class="input_block" type="text"  name="user-add-mid_name" id="user-add-mid_name" placeholder="Введите отчество"/>
+                                </div>
+                                    
+                                <div class="inner-block-add">
+                                    <label for="user-add-last_name" class="label-style">Фамилия:</label>
+                                    <input class="input_block" type="text"  name="user-add-last_name" id="user-add-last_name" placeholder="Введите фамилию"/>
+                                </div>
+                            </div><!-- END DIV BLOCK ADD -->
+                            
+                            <div class="inner-block-add">
+                                <label for="user-add-status" class="label-style" style="text-align: left;">Уровень доступа:</label>
+                                <select id="user-add-status" name="user-add-status" class="input_block2" style="width:250px;">
+                                    <option selected disabled value="0">Выберите уровень доступа</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                             
+                            <input class="submit1" id="btn_sub_add" 
+                            style="margin:0 auto; margin-top:25px; display:none;"  type="button" 
+                            name="submit1" value="Добавить пользователя"
+                            onclick="return validate_form_add_users( );"
+                            />
+                            <input class="submit1" id="btn_sub_edit" 
+                            style="margin:0 auto; margin-top:25px; display:none;"  type="button" 
+                            name="submit1" value="Редактировать данные"
+                            onclick="return validate_form_add_users( );"
+                            />
+                        </form><br/>
+                </div><!--END FORM ADD EDIT -->
+
+                <div id="form_delete" style="display:none;">
+                    <h2>Удалить пользователя</h2><br/>
+
+                    <form name="form3" method="post" action="./php/delete_categories.php" id="form1" style="display:block" onsubmit="return validate_form_categories_delete ( );" >
+                        <input class="input_block" type="text" name="notifications-delete" id="notifications-delete" placeholder="Введите номер строки" />
+                        <br/><br/>
+                        <input class="submit1" type="submit" name="submit1" value="Удалить"/>
+                    </form><br/>
+                </div><!-- END FORM DELETE -->
             </div><!-- END DIV FORM BOX -->
             
             <h2>Пользователи</h2>
