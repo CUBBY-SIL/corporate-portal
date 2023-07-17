1b0772a90af3606d6d8d0cbc65c0e8e08179b4de
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-17 23:25:29 +0700

    Добавил таблицу "Пользователи"

--- sections/section9.php
+++ sections/section9.php
@@ -8,10 +8,26 @@
         
         <script src="./js/jquery-3.6.1.js"></script>
         <script src="./js/jquery.tablesorter.js"></script>
+        <script src="./js/fill_table/fill_users.js"></script>
         
         <div class="inner_content">
             
-        </div>
+        </div><!-- END DIV INNER CONTENT -->
 
+        <div class="table_box" style="overflow-x: hidden;">
+            <table style="border: 1px solid #eee;table-layout: fixed;width: 100%;margin-bottom: 20px; " id="tbl_users" name="tbl_users">  <!--Ширина границ таблицы-->
+                <thead>
+                    <tr>
+                        <th>ID_Записи</th>
+                        <th>Логин</th>
+                        <th>Пароль</th>
+                        <th>Имя</th>
+                        <th>Отчество</th>
+                        <th>Фамилия</th>
+                        <th>Уровень доступа</th>
+                    </tr>
+                </thead>
+            </table>
+        </div><!-- END DIV TABLE BOX -->
     </body>
 </html>
