3ba00ef25af87ac33fe42897506b311003a3568c
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-17 17:11:44 +0700

    Вынес AJAX запрос заполнения таблицы в файл

--- sections/section5.php
+++ sections/section5.php
@@ -9,6 +9,7 @@
         <script src="./js/jquery-3.6.1.js"></script>
         <script src="./js/jquery.tablesorter.js"></script>
         <script src="./js/show_hide_form.js"></script>
+        <script src="./js/fill_table/fill_fbu.js"></script>
         
         <div class="inner_content">
             <div id="form_box">
@@ -111,48 +112,6 @@
                     </tr>
                 </thead>
             </table>
-                
-            <script>
-                $(document).ready(function() {
-                    $.ajax({
-                        url: './php/fetch_data5.php',
-                        method: 'GET',
-                        dataType: 'json',
-                        success: function(data) {
-                            var newLine = $('#tbl_elfbu');
-                            
-                            newLine.append('<tbody>');
-                            
-                            var field = data;
-                            var trHTML = '';
-
-                            $.each(data, function(index, value) {
-                                trHTML += '<tr><td>' + 
-                                    value.id + '</td><td>' + 
-                                    value.date + '</td><td>' + 
-                                    value.name_contr + '</td><td>' + 
-                                    value.name + '</td><td>' + 
-                                    value.term + '</td><td>' + 
-                                    value.fact + '</td></tr>';
-                            });
-                            newLine.append(trHTML);
-                            newLine.append('</tbody>');
-
-                            $("#tbl_elfbu").trigger('updateCache');
-                            $("#tbl_elfbu").tablesorter();
-                        },
-                        error: function(xhr, status, error) {
-                        console.log(xhr.responseText);
-                        }
-                    }); 
-                });
-
-                $(document).ready(function() 
-                    { 
-                            $("#tbl_elfbu").tablesorter(); 
-                    } 
-                );
-            </script>
         </div><!-- END DIV TABLE BOX -->
     </body>
 </html>
