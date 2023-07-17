a65095ed4c8ebeb1df05fdbc0133dd7eee43f593
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-07-17 11:57:57 +0700

    Добавил кнопки для вызова форм обратной связи

--- sections/section2.php
+++ sections/section2.php
@@ -8,10 +8,28 @@
         
         <script src="./js/jquery-3.6.1.js"></script>
         <script src="./js/jquery.tablesorter.js"></script>
+        <script src="./js/show_hide_form.js"></script>
         
         <div class="inner_content">
-            
-        </div>
+            <div id="form_box">
+                <div class="btn_box">
+                    <div id="btn_add">
+                        <button id="btn_ad" class="submit1" >Добавить запись</button>
+                    </div>
+
+                    <div id="btn_edit">
+                        <button id="btn_ed" class="submit1" >Редактировать запись</button>
+                    </div>
+
+                    <div id="btn_delete">
+                        <button id="btn_del" class="submit1" >Удалить запись</button>
+                    </div>
+                </div><br/><!-- END DIV BTN BOX -->
+
+            </div>    
+
+            <h2>Извещения</h2>
+        </div><!-- END DIV INNER CONTENT -->
 
         <div class="table_box">
             <table style="border: 1px solid #eee;table-layout: fixed;width: 100%;margin-bottom: 20px;" id="tbl_izv" name="tbl_izv">
